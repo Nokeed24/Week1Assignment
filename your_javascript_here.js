05fc24f15ec1af0f90ee80cfb76b380c60dcefee
Unknown <nokid_gr@hotmail.it>
2018-04-28 03:46:23 +0200

    Sections 1 and 2 DONE

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -1,5 +1,58 @@
 // Variables
 
-// Game logic
+var hero = {
+  name: "Nick",
+  heroic: true,
+  inventory: [],
+  health: 100,
+  weapon: { type: "Melee", damage: 100}
+};
 
+// Game logic
+function rest(creature) {
+  creature.health = 10;
+  return creature;
+}
+
+function pickUpItem(creature, item) {
+  creature.inventory.push(item);
+  return creature;
+}
+
+function dealDamage(attacker, defender){
+  defender.health -= attacker.weapon.damage;
+  return defender;
+}
+
+function equipWeapon(creature, index){
+  creature.weapon = creature.inventory[index];
+  creature.inventory.splice(index, 1);
+  return creature;
+}
 // UI
+function doBattle(heroicCreature, creature){
+  if (heroicCreature.heroic === true)
+  {
+    while(heroicCreature.health > 0 && creature.health > 0)
+    {
+      dealDamage(heroicCreature, creature);
+      if (creature.health > 0)
+      {
+        dealDamage(creature,heroicCreature);
+      }
+    }
+    if(heroicCreature.health > 0)
+    {
+      return heroicCreature;
+    }
+    else
+    {
+      window.alert("You died... git gud scrub");
+    }
+  }
+  else
+  {
+    return null;
+  }
+
+}
